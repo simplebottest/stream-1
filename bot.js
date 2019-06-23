184da136971172979ec446358277b8e39ee48cfe
simplebottest <46133286+simplebottest@users.noreply.github.com>
2019-06-23 19:25:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,35 +27,6 @@ client.on('ready', () => {
   client.user.setStatus("dnd")
 });
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const bot = new Discord.Client();
-const ms = require("ms");
-const fs = require('fs');
-const moment = require('moment');
-const request = require('request');
-const dateFormat = require('dateformat');
-const r1 = require('snekfetch');
-const jimp = require('jimp')
-const math = require('math-expression-evaluator'); 
-const child_process = require("child_process");
-const developers = ['518113780723351592'];
-const admin = "#";
-const prefix = "#";
-
-
-//=============================== - [ Bot ] - ===================================
-
-
-client.on('ready', () => {
-  console.log(`Logged in as : ${client.user.tag}!`);
-  console.log(`Servers : [ " ${client.guilds.size} " ]`);
-  console.log(`Users : [ " ${client.users.size} " ]`);
-  console.log(`Channels : [ " ${client.channels.size} " ]`);
-  client.user.setActivity("offline",{type: 'WATCHING'})
-  client.user.setStatus("dnd")
-});
-
 
 
 
